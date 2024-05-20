0d1f97391e411365f4f31cbfb294aeade9a28e4b
Markus Schmahl <markus.schmahl@mgm-tp.com>
2024-05-20 13:30:07 +0200

    Task 1: Application Frame

---
+++ helper-files/task1/pages.ts
@@ -0,0 +1,38 @@
+interface PageContent {
+    [pageName: string]: {
+        [locale: string]: string;
+    };
+}
+
+export const defaultPage = "help";
+
+export const pages: PageContent = {
+    help: {
+        en: `
+![ Help page](/images/help_image.jpg)
+# Help Page
+This is a help page.
+`,
+        de: `
+![Hilfeseite](/images/help_image.jpg)
+# Hilfeseite
+Dies ist eine Hilfeseite.
+`
+    },
+    faq: {
+        en: `
+![FAQ page](/images/faq_image.jpg)
+# FAQ Page
+***Is this an FAQ page?***
+
+Yes.
+`,
+        de: `
+![FAQ-Seite](/images/faq_image.jpg)
+# FAQ-Seite
+***Ist das eine FAQ-Seite?***
+
+Ja.
+`
+    }
+};
