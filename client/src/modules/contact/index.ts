ced50fe33cee6e34080bedb8b547be53522617e9
Long Hoàng Phạm <long.hoang.pham@mgm-tp.com>
2024-06-27 09:59:30 +0700

    Apply changes from task 2

--- client/src/modules/contact/index.ts
+++ client/src/modules/contact/index.ts
@@ -1,7 +1,21 @@
+import React from "react";
+
 import { Module } from "@com.mgmtp.a12.client/client-core/lib/core/application";
+import { View } from "@com.mgmtp.a12.client/client-core/lib/core/view";
+
+import HighlightedDateOverview from "./components/HighlightedDateOverview";
+
+const VIEWS: { [name: string]: React.ComponentType<View> | undefined } = {
+    HighlightedDateOverview
+};
+
+function viewComponentProvider(name: string) {
+    return VIEWS[name];
+}
 
 const module = (): Module => ({
-    id: "ContactModule"
+    id: "ContactModule",
+    views: () => viewComponentProvider
 });
 
 export default module;
