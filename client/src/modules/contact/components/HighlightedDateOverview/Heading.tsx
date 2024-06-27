30f2eff21ce72dca1b02b954ce8aba0fdf66b372
Long Hoàng Phạm <long.hoang.pham@mgm-tp.com>
2024-06-27 09:59:30 +0700

    Apply changes from task 2

---
+++ client/src/modules/contact/components/HighlightedDateOverview/Heading.tsx
@@ -0,0 +1,29 @@
+import { ReactElement, PropsWithChildren, Dispatch, SetStateAction } from "react";
+
+import { Heading } from "@com.mgmtp.a12.overviewengine/overviewengine-core/lib/main/view/components/heading";
+import { DefaultComponentMap } from "@com.mgmtp.a12.overviewengine/overviewengine-core/lib/main/view/configuration/component-map";
+import { Button, Icon } from "@com.mgmtp.a12.widgets/widgets-core";
+
+interface HeadingProps extends PropsWithChildren<Heading.PropsType> {
+    isHighlighted: boolean;
+    setIsHighlighted: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function Heading({ isHighlighted, setIsHighlighted, ...headingProps }: HeadingProps): ReactElement {
+    const handleClick = () => {
+        setIsHighlighted((prevIsHighlighted) => !prevIsHighlighted);
+    };
+
+    return (
+        <>
+            <DefaultComponentMap.Heading {...headingProps} />
+            <div className="-u-flex -u-items-center -u-justify-end -u-padding-x-xl -u-margin-y-sm">
+                <Button
+                    onClick={handleClick}
+                    primary
+                    icon={<Icon size="big">{!isHighlighted ? "highlight" : "highlight_off"}</Icon>}
+                />
+            </div>
+        </>
+    );
+}
---
+++ client/src/modules/contact/components/HighlightedDateOverview/index.tsx
@@ -0,0 +1,41 @@
+import { ReactElement, useState, useContext, useMemo, Dispatch, SetStateAction } from "react";
+
+import { View } from "@com.mgmtp.a12.client/client-core/lib/core/view";
+import { CRUDViews } from "@com.mgmtp.a12.crud/crud-core";
+import {
+    ComponentMap,
+    DefaultComponentMap
+} from "@com.mgmtp.a12.overviewengine/overviewengine-core/lib/main/view/configuration/component-map";
+import { SizeContext } from "@com.mgmtp.a12.widgets/widgets-core";
+
+import Heading from "./Heading";
+import TableBodyCell from "./TableBodyCell";
+
+export default function HighlightedDateOverview(props: View): ReactElement {
+    const [isHighlighted, setIsHighlighted] = useState<boolean>(false);
+    const { currentSize } = useContext(SizeContext);
+    const isSmallScreenSize = currentSize === "xs" || currentSize === "sm";
+
+    const componentMap: ComponentMap = useMemo(
+        () => createComponentMap(isHighlighted, setIsHighlighted),
+        [isHighlighted, setIsHighlighted]
+    );
+
+    return <CRUDViews.OverviewEngineView {...props} componentMap={componentMap} cardView={isSmallScreenSize} />;
+}
+
+function createComponentMap(isHighlighted: boolean, setIsHighlighted: Dispatch<SetStateAction<boolean>>): ComponentMap {
+    return {
+        ...DefaultComponentMap,
+        Heading(headingProps) {
+            return <Heading {...headingProps} isHighlighted={isHighlighted} setIsHighlighted={setIsHighlighted} />;
+        },
+        TableBodyCell(tableBodyCellProps) {
+            const { columnModel } = tableBodyCellProps;
+            if (columnModel.id === "column-highlight-date" && isHighlighted) {
+                return <TableBodyCell {...tableBodyCellProps} />;
+            }
+            return <DefaultComponentMap.TableBodyCell {...tableBodyCellProps} />;
+        }
+    };
+}
