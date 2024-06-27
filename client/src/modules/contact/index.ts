471cacb3bae501ebdd2dfd168c2c69f1eda9337a
Long Hoàng Phạm <long.hoang.pham@mgm-tp.com>
2024-06-27 11:06:38 +0700

    Apply changes from task 3

--- client/src/modules/contact/index.ts
+++ client/src/modules/contact/index.ts
@@ -4,9 +4,11 @@ import { Module } from "@com.mgmtp.a12.client/client-core/lib/core/application";
 import { View } from "@com.mgmtp.a12.client/client-core/lib/core/view";
 
 import HighlightedDateOverview from "./components/HighlightedDateOverview";
+import CustomContactForm from "./components/CustomContactForm";
 
 const VIEWS: { [name: string]: React.ComponentType<View> | undefined } = {
-    HighlightedDateOverview
+    HighlightedDateOverview,
+    CustomContactForm
 };
 
 function viewComponentProvider(name: string) {
