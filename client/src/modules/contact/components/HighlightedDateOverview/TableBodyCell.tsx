3ff0d430fffa5b2c229bb03203260801b61423fd
Long Hoàng Phạm <long.hoang.pham@mgm-tp.com>
2024-06-27 10:24:53 +0700

    Task 3: Form customization

--- client/src/modules/contact/components/HighlightedDateOverview/TableBodyCell.tsx
+++ client/src/modules/contact/components/HighlightedDateOverview/TableBodyCell.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, PropsWithChildren } from "react";
-import { addDays, endOfMonth, getYear, isToday, isValid, isWithinInterval, setYear, startOfTomorrow } from "date-fns";
 
 import { DocumentServiceFactory } from "@com.mgmtp.a12.kernel/kernel-md-facade";
 import { TableBodyCell } from "@com.mgmtp.a12.overviewengine/overviewengine-core/lib/main/view/components/table/sub-components/table-body-cell";
@@ -8,32 +7,11 @@ import { GroupInstance } from "@com.mgmtp.a12.kernel/kernel-md-facade/lib/main/j
 import { DefaultComponentMap } from "@com.mgmtp.a12.overviewengine/overviewengine-core/lib/main/view/configuration/component-map";
 import { ReferenceColumn } from "@com.mgmtp.a12.overviewengine/overviewengine-core/lib/main/overview-model";
 
+import { isBirthdayTodayOrNextWeek } from "../../utils/dateUtils";
+
 import HighlightedDateCell from "./HighlightedDateCell";
 
 const documentService = new DocumentServiceFactory().getDocumentService();
-
-function isBirthdayTodayOrNextWeek(birthday: Date): {
-    isBirthdayToday: boolean;
-    isBirthdayNextWeek: boolean;
-} {
-    const today = new Date();
-    let birthdayCurrentYear = setYear(birthday, getYear(today));
-    // Handle leap year
-    if (!isValid(birthdayCurrentYear)) {
-        // Move the date to the end of February
-        birthdayCurrentYear = endOfMonth(new Date(getYear(today), 1));
-    }
-
-    // DoB is today
-    const isBirthdayToday = isToday(birthdayCurrentYear);
-    // DoB is greater than today but less than today plus one week
-    const isBirthdayNextWeek = isWithinInterval(birthdayCurrentYear, {
-        start: startOfTomorrow(),
-        end: addDays(today, 7)
-    });
-
-    return { isBirthdayToday, isBirthdayNextWeek };
-}
 
 export default function TableBodyCell(props: PropsWithChildren<TableBodyCell.Props>): ReactElement {
     const documentModel = useOverviewEngineContext((context) => context.documentModel);
