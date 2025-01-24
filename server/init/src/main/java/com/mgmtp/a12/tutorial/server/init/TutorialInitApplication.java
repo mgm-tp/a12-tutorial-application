c32d68d4ad0e68d81d12276ba45d2d47c7da1deb
Denise Vogel <denise.vogel@mgm-tp.com>
2025-01-25 00:05:29 +0100

    Apply changes from task 2

--- server/init/src/main/java/com/mgmtp/a12/tutorial/server/init/TutorialInitApplication.java
+++ server/init/src/main/java/com/mgmtp/a12/tutorial/server/init/TutorialInitApplication.java
@@ -1,14 +1,14 @@
-package com.mgmtp.a12.template.server.init;
+package com.mgmtp.a12.tutorial.server.init;
 
 import com.mgmtp.a12.dataservices.DataServicesApplication;
 import com.mgmtp.a12.dataservices.init.app.InitAppApplication;
 
 @DataServicesApplication(scanBasePackages = {
         DataServicesApplication.DATASERVICES_BASE_PACKAGE,
-        "com.mgmtp.a12.template.server.init"
+        "com.mgmtp.a12.tutorial.server.init"
 })
-public class ProjectTemplateInitApplication {
+public class TutorialInitApplication {
     public static void main(String[] args) {
-        InitAppApplication.run(args, ProjectTemplateInitApplication.class);
+        InitAppApplication.run(args, TutorialInitApplication.class);
     }
 }
